e3e4ea34dd7990d260f6fcb68f26be67c8d835ee
formation <formation@ubuntu.ubuntu-domain>
2018-03-21 21:00:15 +0100

    test model sqlite

--- agricom/reporter/admin.py
+++ agricom/reporter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Incidences
+
+class IncidencesAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+
+admin.site.register(Incidences,IncidencesAdmin)

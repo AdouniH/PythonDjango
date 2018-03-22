b0c25f14fa2732463ab1b38e4fbeede93ef6c986
formation <formation@ubuntu.ubuntu-domain>
2018-03-22 23:00:49 +0100

    add leaflet & models

--- agricom/reporter/admin.py
+++ agricom/reporter/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
 from .models import Incidences
 
-class IncidencesAdmin(admin.ModelAdmin):
+class IncidencesAdmin(LeafletGeoAdmin):
     list_display = ('name','location')
 
 admin.site.register(Incidences,IncidencesAdmin)
--- agricom/reporter/models.py
+++ agricom/reporter/models.py
@@ -1,9 +1,14 @@
-from django.db import models
+from __future__ import unicode_literals
+
+from django.contrib.gis.db import models
+
+
 
 # Create your models here.
 class Incidences(models.Model):
     name = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
+    location = models.PointField(srid=4326)
+
     def __str__(self):
         return self.name
 
